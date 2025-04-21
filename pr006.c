3197c31f01849daae92e248f5c51355c3f178b60
Rost <dronovfiloposhka@gmail.com>
2025-04-21 17:18:58 +0700

    Добавили вывод переменных среды и разную реакцию программы на аргументы

--- pr006.c
+++ pr006.c
@@ -5,10 +5,19 @@ int main(int argc, char* argv[], char* envp[]) {
 	//Количество аргументов, переданных программе - argc
 	printf("Программе передано %d аргументов\n\n", argc);
 
+	if (argc>=2) {
 	//список значений аргументов (массив argv)
-	for (int  i=0; i<argc; i++)
-		printf("Аргумент [%d]: %s\n", i, argv[i]);
-	printf("\n");
-	
+		for (int  i=0; i<argc; i++)
+			printf("Аргумент [%d]: %s\n", i, argv[i]);
+			printf("\n");
+	}
+	else {
+	//Список значений переменных среды
+		int j=0;
+		while (envp[j]!=NULL) {
+			printf("%d %s\n", j, envp[j]);
+			j++;
+		}
+	}
 }
 	
