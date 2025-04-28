1665d6384a37624a870b0eb06cc59d907c7b931b
Rost <dronovfiloposhka@gmail.com>
2025-04-28 17:08:01 +0700

    Добавили программу для записи байтовой строки в файл

---
+++ pr008.c
@@ -0,0 +1,33 @@
+//программа записи информации из процесса в файл
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+	int fd;
+	size_t size;
+	char string[] = "IFMITO 3.008.2.24 БАРСИК";
+	if ((fd = open("MYFILE", O_WRONLY | O_CREAT, 0666))<0) {
+		printf("Ошибка при открытии файла");
+		exit(-1);
+	}
+
+	size = write(fd,string,31);
+
+	if (size != 31) {
+		printf("Не удалось записать всю строку");
+		exit(-1);
+	}
+
+	printf("В файл с дескриптором %d записано %ld байт\n", fd, size);
+
+	if (close(fd)<0) {
+		printf("Не удается закрыть файл с дескриптором %d\n", fd);
+		exit(-1);
+	}
+
+	return 0;
+
+}
