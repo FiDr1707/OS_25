5a1195751d290758bef40dce28a1669ba4c4890f
Rost <dronovfiloposhka@gmail.com>
2025-04-28 16:10:25 +0700

    Программа, изменяющая пользовательский контекст. Версия 2.

--- pr007.c
+++ pr007.c
@@ -7,8 +7,31 @@
 
 int main(int argc, char *argv[], char *envp[]) {
 	printf("Программа начала работу...\n");
-	(void) execle("/bin/cat", "/bin/cat", "./pr007.c", 0, envp);  
+	pid_t pid, ppid;
+	int result;
+	pid = getpid();
+	ppid = getppid();
+	printf("ID процесса %d\n", pid);
+	printf("ID родительского процесса %d\n", ppid);
+	result = fork();
+
+	if (result > 0) {
+		printf("Родительский процесс ...\n");
+		printf("ID процесса %d\n ID процесса ребенка%d\n", pid, result);
+	       	printf("ID родительского процесса %d\n", ppid);
+		printf("Родитель завершил работу\n");
+	}
+
+	else if (result == 0) {
+	printf("Дочерний процесс ...\n");
+	(void) execle("./pr003.out", "./pr003.out", 0, envp);  
 	printf("Ошибка при выполнении системного вызова exec\n");
 	exit(-1);
+	}
+
+	else {
+	printf("Ошибка при выполнении системного вызова fork\n");
+	exit(-1);
+	}
 	return 0;
 }
