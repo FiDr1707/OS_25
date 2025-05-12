ba4ce96a617a891ea1effa727bc8dab39a85ce0b
Rost <dronovfiloposhka@gmail.com>
2025-05-12 16:30:34 +0700

    Добавили в отчет описание двунаправленной связи через pipe

--- pr009m2.c
+++ pr009m2.c
@@ -3,12 +3,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
-
+#define BYTES1 86
+#define BYTES2 94
 int main() {
 	int fd1[2], fd2[2], result; //fd1 - файловые дескрипторы для передачи информации от родителя к ребенку
 				   //fd2 - файловые дескрипторы для передачи информации от ребенка у родителю
 	size_t size;
-	char resstring1[86], resstring2[94]; //resstring1 - строка, полученная ребенком
+	char resstring1[BYTES1], resstring2[BYTES2]; //resstring1 - строка, полученная ребенком
 					      //resstring2 - строка, полученная родителем
 	if((pipe(fd1)<0)||(pipe(fd2)<0)) {
 		printf("Не удалось создать pipe\n");
@@ -25,8 +26,8 @@ int main() {
 			exit(-1);
 		}
 
-	size = write(fd1[1], "Информация для дочернего процесса от родителя", 86);
-		if(size !=86) {
+	size = write(fd1[1], "Информация для дочернего процесса от родителя", BYTES1);
+		if(size != BYTES1) {
 			printf("Родителю не удалось записать всю строку в pipe\n");
 			exit(-1);
 		}
@@ -36,7 +37,7 @@ int main() {
 		}
 	printf("Читаем строку, отправленную процессом ребенком...\n");
 
-	size = read(fd2[0], resstring2, 94);
+	size = read(fd2[0], resstring2, BYTES2);
 		if(size < 0) {
 			 printf("Родителю не удалось прочитать строку");
 			 exit(-1);
@@ -55,7 +56,7 @@ int main() {
 			exit(-1);
 		}
 
-	size = read(fd1[0], resstring1, 86);
+	size = read(fd1[0], resstring1, BYTES1);
 		if(size < 0) {
 			printf("Не удалось прочитать строку процессу ребенку\n");
 			exit(-1);
@@ -66,8 +67,8 @@ int main() {
 		exit(-1);
 		}
 
-	size = write(fd2[1], "Информация для родительского процесса от родителя", 94);
-		if(size !=94) {
+	size = write(fd2[1], "Информация для родительского процесса от родителя", BYTES2);
+		if(size != BYTES2) {
 			printf("Ребенку не удалось записать всю строку в pipe\n");
 			exit(-1);
 		}
